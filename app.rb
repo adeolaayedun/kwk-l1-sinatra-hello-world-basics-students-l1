49e6710772f1aa8c6f34dc29d90ddc47a23d0961
adeolaayedun <deola.ayedun@gmail.com>
2018-06-12 15:37:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 class App < Sinatra::Base
 
 get '/' do
-  "feellllinnngggggs! so deepp in my feellllinnngggggs! no this ain't really like meeee!"
+  "Hello, World!"
 end
   # Define your GET '/' route below and respond with "Hello, World!"
 
